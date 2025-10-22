0ba7675831e609245dd9db383d5d6258501999e3
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 18:26:53 -0500

    Add About component

---
+++ src/routes/(_index)/About.svelte
@@ -0,0 +1,9 @@
+<div class="w-full py-[10%] px-[10%] md:px-[13%] lg:px-[15%]">
+    <h2 class="text-3xl font-medium md:text-5xl">So... who am I?</h2>
+
+    <div class="mt-10 space-y-4 text-lg">
+        <p>I'm a Computer Engineering student at the University of Alabama in Huntsville. I enjoy solving problems and exploring how code interacts with the real world.</p>
+        <p>In my free time I love to program anything ranging from web applications, discord bots, video games, and much more.</p>
+        <p>I'm always looking for new challenges and opportunities to grow, as there is always something to be learned.</p>
+    </div>
+</div>
