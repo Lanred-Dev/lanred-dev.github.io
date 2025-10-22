e2ae42797eda130bb4c6f96ad32bb364ba56bd6b
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 18:27:05 -0500

    Implement Projects and About components

--- src/routes/(_index)/+page.svelte
+++ src/routes/(_index)/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+    import About from "./About.svelte";
 	import Hero from "./Hero.svelte";
+    import Projects from "./Projects.svelte";
 
 </script>
 
 <Hero />
+<About />
+<Projects />
