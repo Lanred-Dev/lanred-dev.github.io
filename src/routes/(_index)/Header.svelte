f699736e60d4638b85b0ab5932c01934655e1fd4
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 23:56:07 -0500

    Make custom scrollbar appear only after scrolling

--- src/routes/(_index)/Header.svelte
+++ src/routes/(_index)/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
 
     let scrollPercent: number = $state.raw(0);
 
@@ -17,8 +18,9 @@
 
 <div
     class={[
-        "fixed top-10 z-10 flex x-center items-center justify-center gap-5 py-3 px-10 rounded-container sm:gap-15 md:gap-25 lg:gap-35 transition-all duration-300",
-        scrollPercent > 1 && "bg-container/60 shadow-md backdrop-blur-md sm:gap-5! md:gap-10! lg:gap-15!",
+        "x-center rounded-container fixed top-10 z-10 flex items-center justify-center gap-5 px-10 py-3 transition-all duration-300 sm:gap-15 md:gap-25 lg:gap-35",
+        scrollPercent > 1 &&
+            "bg-container/60 shadow-md backdrop-blur-md sm:gap-5! md:gap-10! lg:gap-15!",
     ]}
 >
     {#snippet link(url: string, text: string)}
@@ -30,9 +32,14 @@
     {@render link("#contact", "Contact")}
 </div>
 
-<div class="y-center rounded-container bg-container fixed right-10 z-10 h-28 w-2 overflow-hidden">
+{#if scrollPercent > 5}
     <div
-        class="bg-uah-blue w-full transition-[height] duration-300 ease-in-out"
-        style:height={`${scrollPercent}%`}
-    ></div>
-</div>
+        class="y-center rounded-container bg-container fixed right-10 z-10 h-28 w-2 overflow-hidden"
+        transition:fade
+    >
+        <div
+            class="bg-uah-blue w-full transition-[height] duration-300 ease-in-out"
+            style:height={`${scrollPercent}%`}
+        ></div>
+    </div>
+{/if}
