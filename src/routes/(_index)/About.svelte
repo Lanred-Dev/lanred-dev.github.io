2247ffc2f2bd612d4b5d38173360627074ae61a8
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 20:55:06 -0500

    Refactor About component layout and update personal introduction

--- src/routes/(_index)/About.svelte
+++ src/routes/(_index)/About.svelte
@@ -1,9 +1,7 @@
-<div class="w-full py-[10%] px-[10%] md:px-[13%] lg:px-[15%]">
+<div class="pt-[15%] px-[10%] md:px-[13%] lg:px-[15%] cover-screen">
     <h2 class="text-3xl font-medium md:text-5xl">So... who am I?</h2>
 
-    <div class="mt-10 space-y-4 text-lg">
-        <p>I'm a Computer Engineering student at the University of Alabama in Huntsville. I enjoy solving problems and exploring how code interacts with the real world.</p>
-        <p>In my free time I love to program anything ranging from web applications, discord bots, video games, and much more.</p>
-        <p>I'm always looking for new challenges and opportunities to grow, as there is always something to be learned.</p>
+    <div class="mt-10 space-y-4 text-lg w-full md:pr-[40%]">
+        <p>My name is Landon, I'm a Computer Engineering student at the University of Alabama in Huntsville. I enjoy solving problems and exploring how code interacts with the real world. I'm always looking for new challenges and opportunities to grow, as there is always something to be learned.</p>
     </div>
 </div>
