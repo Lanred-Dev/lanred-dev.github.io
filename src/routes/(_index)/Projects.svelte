f22b69fa9831a3d4c30e17debf7d684252b41270
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 20:54:30 -0500

    Refactor Projects component layout and enhance project rendering details

--- src/routes/(_index)/Projects.svelte
+++ src/routes/(_index)/Projects.svelte
@@ -1,65 +1,69 @@
 <script lang="ts">
 </script>
 
-<div class="w-full px-[10%] py-[10%] md:px-[13%] lg:px-[15%]">
+<div class="cover-screen px-[10%] py-[10%] md:px-[13%] lg:px-[15%]">
     <h2 class="text-3xl font-medium md:text-5xl">What have I done?</h2>
 
-    <div class=" mt-10 grid grid-cols-3 gap-6">
-        {#snippet project(name: string, description: string, link: string)}
-            <div class="p-10 flex flex-col justify-between bg-container rounded-container">
+    <div class="mt-10 grid grid-cols-2 gap-6">
+        {#snippet project(name: string, description: string, tech: string, link: string)}
+            <div class="bg-container rounded-container flex flex-col justify-between p-10">
+                <p class="text-sm mb-0.5 text-secondary">{tech}</p>
                 <h3 class="mb-2 text-2xl font-semibold">{name}</h3>
                 <p class="grow text-lg">{description}</p>
 
-                <a
-                    href={link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button-primary mt-10"
-                >
-                    View Project
-                </a>
+                <div class="mt-8 flex justify-between items-center">
+                    <a href={link} target="_blank" rel="noopener noreferrer"> Live Demo </a>
+                    <a class="button-primary" href={link} target="_blank" rel="noopener noreferrer"> Source </a>
+                </div>
             </div>
         {/snippet}
 
         {@render project(
             "indxo.app",
             "A Quizlet inspired studying website built with SvelteKit.",
+            "SvelteKit, Node.js, MongoDB",
             "https://example.com/personal-portfolio"
         )}
 
         {@render project(
             "extro",
             "A 2D game engine written in Python, using pyray (Raylib) for rendering.",
+            "Python, Raylib, C++",
             "https://github.com/Lanred-Dev/extro"
         )}
 
         {@render project(
             "justreddit",
             "A simple no-dependency Reddit API wrapper for getting post, comment, and subreddit metadata.",
+            "TypeScript, Node.js",
             "https://github.com/Lanred-Dev/justreddit"
         )}
 
         {@render project(
             "Verbose",
             "A simple Discord bot that keeps track of the most used words in a server.",
+            "TypeScript, Discord.js, Node.js",
             "https://github.com/Lanred-Dev/verbose"
         )}
 
         {@render project(
             "Mandelbrot Viewer",
             "A real-time Mandelbrot set viewer written in C++ using OpenGL.",
+            "C++, OpenGL",
             "https://github.com/Lanred-Dev/mandelbrot-viewer"
         )}
 
         {@render project(
             "HarderMC",
             "A Paper plugin that makes Minecraft survival harder with events, stronger mobs, and a fear system.",
+            "Java, PaperMC",
             "https://github.com/Lanred-Dev/HarderMC"
         )}
 
         {@render project(
             "Fluid",
             "Alternative to Roblox's TweenService.",
+            "Lua",
             "https://github.com/Lanred-Dev/Fluid"
         )}
     </div>
