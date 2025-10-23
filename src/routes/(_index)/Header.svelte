d03618133745bc061181237fc002ebc6f48be947
Landon Redmond <landon.redmond0@gmail.com>
2025-10-23 10:14:19 -0500

    Enchance header transition effects

--- src/routes/(_index)/Header.svelte
+++ src/routes/(_index)/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { fade } from "svelte/transition";
+    import { fade, fly } from "svelte/transition";
 
     let scrollPercent: number = $state.raw(0);
 
@@ -18,7 +18,7 @@
 
 <div
     class={[
-        "x-center rounded-container fixed top-10 z-10 flex items-center justify-center gap-5 px-10 py-3 transition-all duration-300 sm:gap-15 md:gap-25 lg:gap-35",
+        "x-center rounded-container fixed top-10 z-10 flex items-center justify-center gap-5 px-10 py-3 transition-all duration-300 will-change-[gap] sm:gap-15 md:gap-25 lg:gap-35",
         scrollPercent > 1 &&
             "bg-container/60 shadow-md backdrop-blur-md sm:gap-5! md:gap-10! lg:gap-15!",
     ]}
@@ -35,7 +35,8 @@
 {#if scrollPercent > 5}
     <div
         class="y-center rounded-container bg-container fixed right-10 z-10 h-28 w-2 overflow-hidden"
-        transition:fade
+        in:fly={{ x: 10, duration: 300 }}
+        out:fade={{ duration: 300 }}
     >
         <div
             class="bg-uah-blue w-full transition-[height] duration-300 ease-in-out"
