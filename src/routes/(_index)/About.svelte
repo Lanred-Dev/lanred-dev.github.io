128e25ae3e585baeae24101ba7a949de84c3238b
Landon Redmond <landon.redmond0@gmail.com>
2025-10-23 23:50:45 -0500

    Update about me section

--- src/routes/(_index)/About.svelte
+++ src/routes/(_index)/About.svelte
@@ -1,7 +1,28 @@
-<div class="pt-[15%] px-[10%] md:px-[13%] lg:px-[15%] cover-screen" id="about">
+<div class="cover-screen px-[10%] pt-[15%] md:px-[13%] lg:px-[15%]" id="about">
     <h2 class="text-3xl font-medium md:text-5xl">So... who am I?</h2>
 
-    <div class="mt-10 space-y-4 text-lg w-full md:pr-[40%]">
-        <p>My name is Landon, I'm a Computer Engineering student at the University of Alabama in Huntsville. I enjoy solving problems and exploring how code interacts with the real world. I'm always looking for new challenges and opportunities to grow, as there is always something to be learned.</p>
+    <div class="mt-10 w-full space-y-4 text-lg md:pr-[35%]">
+        <p>
+            My name is Landon, and I'm a Computer Engineering student at the University of Alabama
+            in Huntsville. I love solving problems and exploring how code interacts with the real
+            world. I'm particularly interested in programming languages like Python, TypeScript,
+            C++, and recently Java. I'm always looking for ways to learn and grow my knowledge, as
+            there is always something to be learned!
+        </p>
+
+        <p>
+            In my free time, I enjoy working on personal projects and learning new technologies (or
+            even making some of my own!). Currently, I'm focused on creating a game engine in Python
+            <a href="https://github.com/lanred-dev/extro" target="_blank" rel="noopener noreferrer"
+                >(extro)</a
+            >
+            and a Quizlet-inspired studying website
+            <a
+                href="https://github.com/indxo-app/index.app"
+                target="_blank"
+                rel="noopener noreferrer">(indxo.app)</a
+            >. My goal with all my projects is to learn and bring to life the ideas that I have to
+            tackle real problems that I've faced.
+        </p>
     </div>
 </div>
