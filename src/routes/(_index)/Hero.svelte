8c63182b636eb330103cf5b773e665958744b3a5
Landon Redmond <landon.redmond0@gmail.com>
2026-07-14 14:52:07 -0500

    Refactor Hero to use backdrop components and improve layout

--- src/routes/(_index)/Hero.svelte
+++ src/routes/(_index)/Hero.svelte
@@ -1,151 +1,77 @@
-<!-- svelte-ignore non_reactive_update -->
 <script lang="ts">
-    import BlurredBlobs from "$lib/Components/BlurredBlobs.svelte";
-    import { onMount } from "svelte";
+    import { SoftBackdrop, SoftBackdropBlob } from "$lib/components/SoftBackdrop";
+    import StarBackdrop from "$lib/components/StarBackdrop.svelte";
+    import Icon from "$lib/components/Icon.svelte";
     import { MediaQuery } from "svelte/reactivity";
+    import { innerWidth, innerHeight } from "svelte/reactivity/window";
 
-    const STAR_COUNT: number = 300;
-    const STAR_SIZE: [number, number] = [0.5, 1.5];
-    const FPS: number = 30;
-    const STAR_OPACITY: number = 0.3;
-
-    interface Star {
-        x: number;
-        y: number;
-        radius: number;
-        opacity: number;
-        isAnimating: boolean;
-        opacityDirection: number;
-        fadeSpeed: number;
-    }
-
-    let delta: number;
-    let lastFrameTime: number = Date.now();
-    let Canvas: HTMLCanvasElement;
-    let context: CanvasRenderingContext2D;
-    let BlobsCanvas: HTMLCanvasElement;
-    let canvasWidth: number = $state.raw(0);
-    let canvasHeight: number = $state.raw(0);
-    let stars: Star[] = [];
     // 768px is the same sizing used for md: in tailwind
     const isMobile: MediaQuery = new MediaQuery("(max-width: 768px)", true);
-
-    function getRandomPosition() {
-        return {
-            x: Math.random() * canvasWidth,
-            y: Math.random() * canvasHeight,
-        };
-    }
-
-    function draw() {
-        delta = Date.now() - lastFrameTime;
-        lastFrameTime = Date.now();
-
-        context.clearRect(0, 0, canvasWidth, canvasHeight);
-
-        stars.forEach((star) => {
-            if (star.isAnimating) {
-                star.opacity += star.fadeSpeed * star.opacityDirection * delta;
-
-                if (star.opacity >= 1) {
-                    star.opacity = 1;
-                    star.isAnimating = false;
-                    star.opacityDirection = 0;
-                } else if (star.opacity <= 0) {
-                    star.opacity = 0;
-                    star.opacityDirection = 1;
-
-                    const { x, y } = getRandomPosition();
-                    star.x = x;
-                    star.y = y;
-                }
-            } else if (Math.random() < 0.1) {
-                star.isAnimating = true;
-                star.opacityDirection = star.opacity === 1 ? -1 : 1;
-            }
-
-            context.globalAlpha = star.opacity * STAR_OPACITY;
-            context.beginPath();
-            context.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-            context.fillStyle = "#FFFFFF";
-            context.fill();
-        });
-
-        context.globalAlpha = 0.5;
-        context.drawImage(BlobsCanvas, 0, 0, canvasWidth, canvasHeight);
-    }
-
-    function generateStars() {
-        stars = [];
-
-        for (let index = 0; index < STAR_COUNT; index++) {
-            stars.push({
-                ...getRandomPosition(),
-                radius: Math.random() * (STAR_SIZE[1] - STAR_SIZE[0]) + STAR_SIZE[0],
-                opacity: Math.random(),
-                isAnimating: false,
-                opacityDirection: 0,
-                fadeSpeed: Math.random() * 0.003 + 0.0003,
-            });
-        }
-    }
-
-    onMount(() => {
-        context = Canvas.getContext("2d") as CanvasRenderingContext2D;
-
-        generateStars();
-
-        requestAnimationFrame(draw);
-        const renderInterval = setInterval(() => {
-            requestAnimationFrame(draw);
-        }, 1000 / FPS);
-
-        return () => {
-            clearInterval(renderInterval);
-        };
-    });
-
-    $effect(() => {
-        if (!Canvas) return;
-
-        const ratio: number = Math.ceil(window.devicePixelRatio);
-        Canvas.width = canvasWidth * ratio;
-        Canvas.height = canvasHeight * ratio;
-        context.setTransform(ratio, 0, 0, ratio, 0, 0);
-
-        generateStars();
-    });
 </script>
 
-<BlurredBlobs
-    blobs={[
-        {
-            x: 0.05,
-            y: 0.9,
-            radius: 400,
-            color: "#003DA5",
-            opacity: 0.2,
-        },
-        {
-            x: 0.9,
-            y: 0.5,
-            radius: 300,
-            color: "#fdda24",
-            opacity: 0.1,
-        },
-    ]}
-    width={canvasWidth}
-    height={canvasHeight}
-    bind:Canvas={BlobsCanvas}
-/>
-
 <div class="cover-screen relative overflow-hidden">
-    <canvas
-        class="y-center x-center h-screen w-full"
-        bind:this={Canvas}
-        bind:clientWidth={canvasWidth}
-        bind:clientHeight={canvasHeight}
-    ></canvas>
+    <div class="absolute top-0 left-0 z-0 h-full w-full">
+        <StarBackdrop
+            class="absolute top-0 left-0 z-1 h-full w-full"
+            width={innerWidth.current ?? 0}
+            height={innerHeight.current ?? 0}
+        />
+
+        <SoftBackdrop
+            class="absolute top-0 left-0 z-2 h-full w-full"
+            width={innerWidth.current ?? 0}
+            height={innerHeight.current ?? 0}
+        >
+            <SoftBackdropBlob x={0.05} y={0.9} radius={400} color="#003DA5" opacity={0.1} />
+            <SoftBackdropBlob x={0.9} y={0.5} radius={300} color="#fdda24" opacity={0.05} />
+        </SoftBackdrop>
+
+        <div
+            class="w-full h-full absolute top-0 left-0 z-3 bg-no-repeat bg-cover"
+            style="background: linear-gradient(to top, var(--background-color-page) 0%, transparent 30%)"
+        ></div>
+
+        <div class="x-center bottom-15 z-4 size-7 opacity-70">
+            <Icon
+                icon="general/chevrons/Down"
+                class="h-full w-full fill-white"
+                style="animation: Arrow 3.5s infinite cubic-bezier(0.25, 1, 0.5, 1);"
+            />
+
+            <style lang="postcss">
+                @keyframes Arrow {
+                    0%,
+                    40% {
+                        opacity: 1;
+                        transform: translateY(0);
+                    }
+                    45% {
+                        opacity: 0;
+                        transform: translateY(20px);
+                    }
+                    46% {
+                        opacity: 0;
+                        transform: translateY(-20px);
+                    }
+                    55% {
+                        opacity: 1;
+                        transform: translateY(8px);
+                    }
+                    62% {
+                        transform: translateY(-3px);
+                    }
+                    68% {
+                        transform: translateY(1px);
+                    }
+                    73%,
+                    100% {
+                        opacity: 1;
+                        transform: translateY(0);
+                    }
+                }
+            </style>
+        </div>
+    </div>
 
     <div class="lg:y-center absolute top-[30%] left-[6%] z-2 md:left-[13%] lg:left-[15%]">
         <h1 class="text-4xl font-medium md:text-5xl">Hi, I'm Landon</h1>
@@ -154,7 +80,7 @@
         </p>
     </div>
 
-    <div class="md:y-center absolute right-0 bottom-[10%] z-2 h-[50%] w-full md:h-dvh md:w-[40%]">
+    <div class="md:y-center absolute right-0 bottom-[10%] z-3 h-[50%] w-full md:h-dvh md:w-[40%]">
         {#snippet profileLink(
             x: number,
             y: number,
@@ -167,38 +93,33 @@
             <a
                 {href}
                 target="_blank"
-                rel="noopener noreferrer"
-                class="profileLink group absolute flex size-8 -translate-x-1/2 -translate-y-1/2 items-center gap-2 transition-transform duration-200 hover:scale-110"
+                rel="external noopener noreferrer"
+                class="group absolute -translate-x-1/2 -translate-y-1/2 transition-transform duration-200 hover:scale-110"
                 aria-label={alt}
                 style:right="{isMobile.current ? dx : x}%"
                 style:top="{isMobile.current ? dy : y}%"
             >
-                <img src={icon} {alt} class="aspect-square w-full" />
+                <Icon
+                    {icon}
+                    class="size-8 fill-white"
+                    style="filter: drop-shadow(0px 0px 3px #ffffff80);"
+                />
+
                 <p
-                    class="translate-y-0.5 opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                    class="-translate-y-1/4 opacity-0 transition-[translate,opacity] group-hover:-translate-y-1/2 group-hover:opacity-100 absolute top-1/2 left-10"
                 >
                     {alt}
                 </p>
             </a>
         {/snippet}
 
-        <style lang="postcss">
-            .profileLink img {
-                filter: drop-shadow(0px 0px 0px #ffffff80);
-            }
-
-            .profileLink:hover img {
-                filter: drop-shadow(0px 0px 10px #ffffff80);
-            }
-        </style>
-
         {@render profileLink(
             42,
             41,
             42,
             41,
-            "/icons/GitHub.png",
-            "Github",
+            "logos/GitHub",
+            "GitHub",
             "https://github.com/lanred-dev"
         )}
 
@@ -207,59 +128,11 @@
             73,
             15,
             73,
-            "/icons/Mail.svg",
+            "general/Mail",
             "Email",
             "mailto:landon.redmond0@gmail.com"
         )}
 
-        {@render profileLink(82, 28, 76, 70, "/icons/Resume.svg", "Resume", "/Resume.pdf")}
+        {@render profileLink(82, 28, 76, 70, "general/Resume", "Resume", "/Resume.pdf")}
     </div>
-
-    <div class="x-center bottom-15 z-2 size-7 opacity-70">
-        <img
-            src="/icons/ChevronDown.svg"
-            alt="Down Arrow"
-            class="aspect-square w-full"
-            style:animation="Arrow 5s infinite ease-in-out"
-        />
-
-        <style lang="postcss">
-            @keyframes Arrow {
-                0%,
-                40% {
-                    opacity: 1;
-                    transform: translateY(0px);
-                }
-
-                47% {
-                    opacity: 0;
-                }
-
-                48% {
-                    transform: translateY(15px);
-                }
-
-                49% {
-                    transform: translateY(-15px);
-                }
-
-                87% {
-                    transform: translateY(-15px);
-                }
-
-                89% {
-                    opacity: 0;
-                }
-
-                100% {
-                    opacity: 1;
-                    transform: translateY(0px);
-                }
-            }
-        </style>
-    </div>
-
-    <div
-        class="from-page absolute bottom-0 left-0 z-1 h-1/8 w-full bg-linear-to-t to-transparent"
-    ></div>
 </div>
