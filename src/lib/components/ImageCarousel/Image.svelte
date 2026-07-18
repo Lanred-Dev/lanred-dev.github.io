9578fcda86fce71e817b3df0925b9903e2700636
Landon Redmond <landon.redmond0@gmail.com>
2026-07-17 19:42:59 -0500

    Refactor ImageCarousel component to use ExpandableImage and improve transition handling

--- src/lib/components/ImageCarousel/Image.svelte
+++ src/lib/components/ImageCarousel/Image.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
     import type { ClassValue } from "svelte/elements";
     import { imageCarouselKey, type ImageCarouselContext } from ".";
-    import { getContext } from "svelte";
-    import { fly } from "svelte/transition";
+    import { getContext, onMount } from "svelte";
+    import ExpandableImage from "../ExpandableImage.svelte";
 
     let {
         class: className,
         image,
-        transitionInX = 30,
-        transitionOutX = -30,
+        transitionInX = "30px",
+        transitionOutX = "-30px",
         transitionDuration = 800,
     }: {
         class?: ClassValue;
         image: string;
-        transitionInX?: string | number;
-        transitionOutX?: string | number;
+        transitionInX?: string;
+        transitionOutX?: string;
         transitionDuration?: number;
     } = $props();
 
     const imageCarousel: ImageCarouselContext = getContext(imageCarouselKey);
     let index: number = $state.raw(imageCarousel.registerImage());
+    let xPosition: string = $derived.by(() => {
+        if (index > imageCarousel.currentImageIndex) return transitionInX;
+        if (index < imageCarousel.currentImageIndex) return transitionOutX;
+        return "0px";
+    });
+
+    onMount(() => {
+        const imagePreload = new Image();
+        imagePreload.src = image;
+        imagePreload.onload = () => {
+            imagePreload.remove();
+        };
+    });
 
     $effect(() => {
         if (transitionDuration > imageCarousel.highestTransitionDuration)
@@ -27,15 +40,17 @@
     });
 </script>
 
-{#if imageCarousel.currentImageIndex === index}
-    <img
+<div
+    class={["w-full h-full absolute top-0 left-0 translate-[translate,opacity]", className]}
+    style:z-index={index > imageCarousel.currentImageIndex ? 1 : 0}
+    style:opacity={index === imageCarousel.currentImageIndex ? 1 : 0}
+    style:transition-duration="{transitionDuration}ms"
+    style:translate="{xPosition} 0"
+>
+    <ExpandableImage
         src={image}
         alt="Carousel Image {index + 1}"
-        class={["w-full h-full object-cover absolute top-0 left-0", className]}
-        style:z-index={index > imageCarousel.currentImageIndex ? 1 : 0}
-        transition:fly={{
-            x: imageCarousel.currentImageIndex !== index ? transitionOutX : transitionInX,
-            duration: transitionDuration,
-        }}
+        class="w-full h-full object-cover"
+        triggerClass="w-full h-full"
     />
-{/if}
+</div>
