619f8d7fbedf27b75e8f842386dfe6abc79d7542
Landon Redmond <landon.redmond0@gmail.com>
2026-07-20 22:53:37 -0500

    Improve Header responsiveness on mobile

--- src/routes/(_index)/Header.svelte
+++ src/routes/(_index)/Header.svelte
@@ -7,14 +7,14 @@
 
 <div
     class={[
-        "x-center fixed top-10 z-10 grid grid-cols-3 items-center justify-center gap-12 rounded-full px-10 py-3 transition-all duration-300 will-change-[gap] md:gap-25 lg:gap-35",
+        "x-center fixed top-10 z-10 grid grid-cols-3 w-max items-center justify-center gap-8 sm:gap-12 rounded-full px-5 md:px-10 py-3 transition-all duration-300 will-change-[gap] md:gap-25 lg:gap-35",
         viewportContext.scrollY > 1 &&
             "bg-container/60 gap-8! shadow-md backdrop-blur-md md:gap-12! lg:gap-15!",
     ]}
 >
     {#snippet link(element: string, text: string)}
         <button
-            class="text-lg opacity-30 transition-opacity hover:opacity-100"
+            class="text-lg opacity-30 transition-opacity hover:opacity-100 w-full text-center"
             onclick={() => scrollToElement(element)}>{text}</button
         >
     {/snippet}
