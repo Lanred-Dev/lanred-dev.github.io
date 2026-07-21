d5896de0917bb26ee84cd32dc7a7ff33229e0f91
Landon Redmond <landon.redmond0@gmail.com>
2026-07-20 23:02:22 -0500

    Refactor scrollbar drag handling to support touch events

--- src/routes/Scrollbar.svelte
+++ src/routes/Scrollbar.svelte
@@ -16,13 +16,14 @@
     let isHoldingThumb: boolean = $state.raw(false);
     let Track: HTMLElement | null = $state(null);
 
-    function handleMouseMove(event: MouseEvent, animate: boolean) {
+    function handleDrag(event: MouseEvent | TouchEvent, animate: boolean) {
         if (!Track) return;
 
-        const scrollbarBounding = Track.getBoundingClientRect();
-        const clickY = event.clientY - scrollbarBounding.top;
-        const scrollRatio = Math.min(1, Math.max(0, clickY / scrollbarBounding.height));
-        const newScrollY =
+        const clientY: number = "touches" in event ? event.touches[0].clientY : event.clientY;
+        const scrollbarBounding: DOMRect = Track.getBoundingClientRect();
+        const clickY: number = clientY - scrollbarBounding.top;
+        const scrollRatio: number = Math.min(1, Math.max(0, clickY / scrollbarBounding.height));
+        const newScrollY: number =
             scrollRatio * (viewportContext.Content.scrollHeight - viewportContext.height);
 
         if (!animate) {
@@ -35,9 +36,14 @@
 
 <svelte:window
     onmousemove={(event) => {
-        if (isHoldingThumb) handleMouseMove(event, false);
+        if (isHoldingThumb) handleDrag(event, false);
+    }}
+    ontouchmove={(event) => {
+        if (isHoldingThumb) handleDrag(event, false);
     }}
     onmouseup={() => (isHoldingThumb = false)}
+    ontouchend={() => (isHoldingThumb = false)}
+    ontouchcancel={() => (isHoldingThumb = false)}
 />
 
 {#if viewportContext.scrollY > 1 || isHoldingThumb}
@@ -49,17 +55,19 @@
         <button
             class="w-full h-full x-center y-center bg-container hover:scale-x-150 transition-transform rounded-full"
             aria-label="Scrollbar track"
-            onclick={(event) => handleMouseMove(event, true)}
+            onclick={(event) => handleDrag(event, true)}
             bind:this={Track}
         >
         </button>
 
         <button
-            class="bg-scrollbar w-full h-1/14 absolute rounded-full transition-[scale,opacity] duration-300 z-1 ease-in-out hover:scale-180"
+            class="bg-scrollbar w-full h-1/14 absolute rounded-full transition-transform duration-300 z-1 ease-in-out hover:scale-180"
+            style:scale={isHoldingThumb ? 1.8 : undefined}
             style:top="calc({scrollPercent}% - ({thumbHeight}px * {scrollPercent} / 100))"
             bind:clientHeight={thumbHeight}
             aria-label="Scrollbar thumb"
             onmousedown={() => (isHoldingThumb = true)}
+            ontouchstart={() => (isHoldingThumb = true)}
         ></button>
     </div>
 {/if}
